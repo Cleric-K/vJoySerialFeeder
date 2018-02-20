4c97780ec4f72588cd54ee5e8d52b9d8aec3a26f
Cleric-K <9365881+Cleric-K@users.noreply.github.com>
2018-02-20 22:40:37 +0200

    Updated Arduino example for Bitmapped Button

--- Arduino/Joystick/Joystick.ino
+++ Arduino/Joystick/Joystick.ino
@@ -4,19 +4,37 @@
 // //////////////////
 // Edit here to customize
 
+// How often to send data?
 #define UPDATE_INTERVAL 10 // milliseconds
 
-// The number of analog channels you would like to read
-#define NUM_ANALOG_CHANNELS 4
 
-// Enter the the analog pins you would like to read. Their count must be NUM_ANALOG_CHANNELS
-byte analogPins[] = {0, 1, 2, 3};
+// 1. Analog channels. Data can be read with the Arduino's 10-bit ADC.
+// This gives values from 0 to 1023.
+// Specify below the analog pin numbers (as for analogRead) you would like to use.
+// Every analog input is sent as a single channel.
+#define NUM_ANALOG_INPUTS 4
+byte analogPins[] = {0, 1, 2, 3}; // element count MUST be == NUM_ANALOG_INPUTS
 
-// The number of digital channels you would like to red
-#define NUM_DIGITAL_CHANNELS 2
 
-// Enter the the digital pins you would like to read. Their count must be NUM_DIGITAL_CHANNELS
-byte digitalPins[] = {6, 7};
+// 2. Digital channels. Data can be read from Arduino's digital pins.
+// They could be either LOW or HIGH.
+// Specify below the digital pin numbers (as for digitalRead) you would like to use.
+// Every pin is sent as a single channel. LOW is encoded as 0, HIGH - as 1023
+#define NUM_DIGITAL_INPUTS 2
+byte digitalPins[] = {6, 7}; // element count MUST be == NUM_DIGITAL_INPUTS
+
+
+// 3. Digital bit-mapped channels. Sending a single binary state as a 16-bit
+// channel is pretty wasteful. Instead, we can encode one digital input
+// in each of the 16 channel bits.
+// Specify below the digital pins (as for digitalRead) you would like to send as
+// bitmapped channel data. Data will be automatically organized in channels.
+// The first 16 pins will go in one channel (the first pin goes into the LSB of the channel).
+// The next 16 pins go in another channel and so on
+// LOW pins are encoded as 0 bit, HIGH - as 1.
+#define NUM_DIGITAL_BITMAPPED_INPUTS 3
+byte digitalBitmappedPins[] = {8, 9, 10}; // element count MUST be == NUM_DIGITAL_BITMAPPED_INPUTS
+
 
 // Define the appropriate analog reference source. See
 // https://www.arduino.cc/reference/en/language/functions/analog-io/analogreference/
@@ -31,8 +49,7 @@ byte digitalPins[] = {6, 7};
 
 
 
-
-#define NUM_CHANNELS (NUM_ANALOG_CHANNELS + NUM_DIGITAL_CHANNELS)
+#define NUM_CHANNELS ( (NUM_ANALOG_INPUTS) + (NUM_DIGITAL_INPUTS) + (15 + (NUM_DIGITAL_BITMAPPED_INPUTS))/16 )
 
 
 IBus ibus(NUM_CHANNELS);
@@ -45,15 +62,31 @@ void setup()
 
 void loop()
 {
-  int i;
+  int i, bm_ch = 0;
   unsigned long time = millis();
-  
+
   ibus.begin();
-  
-  for(i=0; i < NUM_ANALOG_CHANNELS; i++)
+
+  // read analog pins - one per channel
+  for(i=0; i < NUM_ANALOG_INPUTS; i++)
     ibus.write(analogRead(analogPins[i]));
-  for(i=0; i < NUM_DIGITAL_CHANNELS; i++)
+
+  // read digital pins - one per channel
+  for(i=0; i < NUM_DIGITAL_INPUTS; i++)
     ibus.write(digitalRead(digitalPins[i]) == HIGH ? 1023 : 0);
+
+  // read digital bit-mapped pins - 16 pins go in one channel
+  for(i=0; i < NUM_DIGITAL_BITMAPPED_INPUTS; i++) {
+  	int bit = i%16;
+  	if(digitalRead(digitalBitmappedPins[i]) == HIGH)
+  		bm_ch |= 1 << bit;
+
+  	if(bit == 15 || i == NUM_DIGITAL_BITMAPPED_INPUTS-1) {
+  		// data for one channel ready
+  		ibus.write(bm_ch);
+  		bm_ch = 0;
+  	}
+  }
 
   ibus.end();
 
