ad26f27c26053a86f893ff8738469123b6b71cea
Cleric-K <9365881+Cleric-K@users.noreply.github.com>
2018-03-02 11:21:00 +0200

    SbusReader updated after merge
    Thanks to @fape (see #3) for his help on testing.

--- vJoySerialFeeder/SbusReader.cs
+++ vJoySerialFeeder/SbusReader.cs
@@ -5,27 +5,58 @@
  * Time: 12:57 ч.
  */
 using System;
+using System.IO.Ports;
 
 namespace vJoySerialFeeder
 {
 	/// <summary>
-	/// Description of SbusReader.
+	/// Taken from: https://os.mbed.com/users/Digixx/notebook/futaba-s-bus-controlled-by-mbed/
+	/// 
+	/// The protocol is 25 Byte long and is send every 14ms (analog mode) or 7ms (highspeed mode).
+	///	One Byte = 1 startbit + 8 databit + 1 paritybit + 2 stopbit (8E2), baudrate = 100'000 bit/s
+	///	The highest bit is send first. The logic is inverted (Level High = 1)
+	///	
+	///	[startbyte] [data1] [data2] .... [data22] [flags][endbyte]
+	///	
+	///	startbyte = 11110000b (0xF0)
+	///	
+	///	data 1-22 = [ch1, 11bit][ch2, 11bit] .... [ch16, 11bit] (ch# = 0 bis 2047)
+	///	channel 1 uses 8 bits from data1 and 3 bits from data2
+	///	channel 2 uses last 5 bits from data2 and 6 bits from data3
+	///	etc.
+	///	
+	///	flags = bit7 = ch17 = digital channel (0x80)
+	///	bit6 = ch18 = digital channel (0x40)
+	///	bit5 = Frame lost, equivalent red LED on receiver (0x20)
+	///	bit4 = failsafe activated (0x10)
+	///	bit3 = n/a
+	///	bit2 = n/a
+	///	bit1 = n/a
+	///	bit0 = n/a
+	///	
+	///	endbyte = 00000000b
 	/// </summary>
+	/// 
 	public class SbusReader : SerialReader
-	{		
+	{
+		private const byte FIRST_BYTE = 0x0f;
+		private const byte LAST_BYTE = 0x00;
+		private const int FRAME_LENGTH = 25; // sbus frame is always 25 bytes
+	
+		public override void Start()
+		{
+			Buffer.FrameLength = FRAME_LENGTH;
+			serialPort.ReadTimeout = 500;
+		}
+		
 		public override void Stop()
 		{
 		}
 		
-		public override void Start()
-		{
-			Buffer.FrameLength = 25; // sbus frame is always 25 bytes
-			serialPort.ReadTimeout = 500;
-		}
 		
 		public override int ReadChannels()
 		{
-			if(Buffer[0] != 0x0f || Buffer[24] != 0x00) {
+			if(Buffer[0] != FIRST_BYTE || Buffer[24] != LAST_BYTE) {
 				// first and last bytes incorrect, try resyncing
 				System.Diagnostics.Debug.WriteLine("resyncing");
 				Buffer.Slide(1);
@@ -53,8 +84,20 @@ namespace vJoySerialFeeder
 			
 			// do not check the flags byte, we don't really need anything from there
 			
-			Buffer.Slide(25);
+			Buffer.Slide(FRAME_LENGTH);
 			return 16;
+		}
+		
+		public override Configuration.SerialParameters GetDefaultSerialParameters()
+		{
+			Configuration.SerialParameters p;
+			
+			p.BaudRate = 100000;
+			p.DataBits = 8;
+			p.Parity = Parity.Even;
+			p.StopBits = StopBits.Two;
+			
+			return p;
 		}
 	}
 }
