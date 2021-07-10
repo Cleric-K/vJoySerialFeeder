1dc31f977cafc6b0155dbb73502e74c12e2d3d6e
Chok <8801373+cmyip@users.noreply.github.com>
2021-07-10 13:46:58 +0800

    Update to matching .net framework for vJoy interface

--- vJoySerialFeeder/Settings.Designer.cs
+++ vJoySerialFeeder/Settings.Designer.cs
@@ -9,30 +9,30 @@
 //------------------------------------------------------------------------------
 
 namespace vJoySerialFeeder {
-	
-	
-	[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("ICSharpCode.SettingsEditor.SettingsCodeGeneratorTool", "4.4.2.9749")]
-	internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-		
-		private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-		
-		public static Settings Default {
-			get {
-				return defaultInstance;
-			}
-		}
-		
-		[global::System.Configuration.UserScopedSettingAttribute()]
-		[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-		[global::System.Configuration.DefaultSettingValueAttribute("")]
-		public string config {
-			get {
-				return ((string)(this["config"]));
-			}
-			set {
-				this["config"] = value;
-			}
-		}
-	}
+    
+    
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string config {
+            get {
+                return ((string)(this["config"]));
+            }
+            set {
+                this["config"] = value;
+            }
+        }
+    }
 }
